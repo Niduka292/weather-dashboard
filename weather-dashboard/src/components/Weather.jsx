639b29311e780705747bef960e2c161b48d5d2da
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2026-04-10 23:01:12 +0530

    Make the application responsive for user input

--- weather-dashboard/src/components/Weather.jsx
+++ weather-dashboard/src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -10,31 +10,96 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
 const Weather = () => {
+
+    const inputRef = useRef();
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon
+    }
+
+    const search = async (city) => {
+
+        if(city === ""){
+            alert("Enter a city");
+            return;
+        }
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if(!response.ok){
+                alert(data.message);
+                return;
+            }
+
+            const icon = allIcons[data.weather[0].icon] || clear_icon;
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp - 273),
+                location: data.name,
+                icon: icon
+            });
+            console.log("Humidity: ", weatherData.humidity);
+
+
+        } catch (error) {
+            setWeatherData(false);
+            console.error("Error fetching weather data");
+            
+        }
+    }
+
+    useEffect(() => {
+        search("Moscow");
+    }, [])
+
+
     return (
         <div className="weather">
             <div className="search-bar">
-                <input type="text" placeholder="Search" />
-                <img src={search_icon} alt="" />
+                <input ref={inputRef} type="text" placeholder="Search" />
+                <img src={search_icon} alt="" onClick={() => search(inputRef.current.value)} />
             </div>
-            <img src={clear_icon} alt='' className='weather-icon' />
-            <p className='temperature'>16°C</p>
-            <p className='city'>London</p>
-            <div className='weather-data'>
-                <div className='col'>
-                    <img src={humidity_icon} alt=''/>
-                    <div>
-                        <p className='humidity'>91%</p>
-                        <span>Humidity</span>
+            {weatherData ? <>
+
+                <img src={weatherData.icon} alt='' className='weather-icon' />
+                <p className='temperature'>{weatherData.temperature}°C</p>
+                <p className='city'>{weatherData.location}</p>
+                <div className='weather-data'>
+                    <div className='col'>
+                        <img src={humidity_icon} alt='' />
+                        <div>
+                            <p className='humidity'>{weatherData.humidity}%</p>
+                            <span>Humidity</span>
+                        </div>
+                    </div>
+                    <div className='col'>
+                        <img src={wind_icon} alt='' />
+                        <div>
+                            <p className='wind'>{weatherData.windSpeed} km/h</p>
+                            <span>Wind Speed</span>
+                        </div>
                     </div>
                 </div>
-                <div className='col'>
-                    <img src={wind_icon} alt=''/>
-                    <div>
-                        <p className='wind'>3.5 km/h</p>
-                        <span>Wind Speed</span>
-                    </div>
-                </div>
-            </div>
+            </> : <></>}
+
         </div>
     )
 }
